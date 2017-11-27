9b4aa48eda0802ada0b8b9fe22717f5b894802d2
Alex <olefirenk@gmail.com>
2017-11-27 23:54:46 +0200

    fix: exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 export {default as Fetcher} from './Fetcher'
 export {default as Settings} from './Settings'
+export {default as Cookies} from './Cookies'
 export {fetcher, stringifyQuery, parseQuery} from './utils/fetcher'
 export {fetcher as default} from './utils/fetcher'
-export {
-    plugToRequest as plugCookieToRequest
-} from './utils/cookies'
+
 export {
     isAuthenticated,
     logout,
