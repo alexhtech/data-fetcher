fdf9c0c6c449a566585ce69e2484fdea90bd8f25
Alex <a.olefirenko@besk.com>
2018-06-08 23:37:59 +0300

    remove unnecessary files

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-import Fetcher, { defaults } from './Fetcher';
-const { fetcher, config, stringifyQuery, parseQuery } = new Fetcher();
-export { defaults };
-export { fetcher, config, stringifyQuery, parseQuery, fetcher as default };
--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import { IConfig, IOptions, defaults, IArgs } from './Fetcher';
-declare const fetcher: (url: string, options?: IOptions, catchError?: boolean) => Promise<any>, config: IConfig, stringifyQuery: (params?: object | undefined) => string, parseQuery: (queryString: string) => any;
-export { IArgs, IOptions, IConfig, defaults };
-export { fetcher, config, stringifyQuery, parseQuery, fetcher as default };
